5dbe7fc70ac4ee6b3c01487e08b32b5b03cf3c96
Krzysztof Urban <krzysztof.urban@netguru.pl>
2012-01-26 18:23:37 +0100

    ugly, but I don't care

--- app.py
+++ app.py
@@ -1,20 +1,56 @@
 # -*- coding: utf-8 -*-
+from collections import defaultdict
+
+import json
+import time
 
 import reddit
 
-config = {'subreddits': ['politics', 'minecraft'], 'post_limit': 30}
+config = {'sleep_time': 30, 'limit': 500}
+
+file_template = 'dump/{0}_{1}.json'
+file_numbering = 0
+
+def save_as_json(dic, name, number):
+    filename = file_template.format(name, number)
+    data = json.dumps(dic, sort_keys=True, indent=2)
+    with open(filename, 'w') as f:
+        f.write(data)
 
 def app():
-    users = []
+    global file_numbering
     r = reddit.Reddit(user_agent='karmelzebra')
-    for subreddit_name in config['subreddits']:
-        subreddit = r.get_subreddit(subreddit_name)
-        submissions = subreddit.get_hot(limit=config['post_limit'])
-        for sub in submissions:
-            users.append(sub.author)
 
-    for user in users:
-        print user
+    while file_numbering < 500:
+        print "Requesting batch nr {0}".format(file_numbering)
+        try:
+            names = defaultdict(list)
+            reddits = defaultdict(list)
+            comments = r.get_all_comments(limit=config['limit'], place_holder=None)
+
+            comment_count = 0
+            for comment in comments:
+                names[str(comment.author)].append(str(comment.subreddit))
+                reddits[str(comment.subreddit)].append(str(comment.author))
+                comment_count += 1
+
+            save_as_json(names, 'names', file_numbering)
+            save_as_json(reddits, 'reddits', file_numbering)
+            print "Batch nr {0} saved, {1} new records.".format(file_numbering, comment_count)
+        except KeyboardInterrupt:
+            raise
+        except:
+            print "Unexpected error"
+            time.sleep(config['sleep_time'])
+            continue
+        finally:
+            file_numbering += 1
+
+        print "Sleeping now for {0} seconds...".format(config['sleep_time'])
+        time.sleep(config['sleep_time'])
 
 if __name__ == '__main__':
-    app()
+    try:
+        app()
+    except KeyboardInterrupt:
+        print "\nDownloading stopped by user. Bye!"
